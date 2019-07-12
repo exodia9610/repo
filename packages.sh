2b965f4a025bad7c22c3a540a8d711567c0985c8
exodia9610 <noahlittle199@gmail.com>
2019-07-12 17:48:38 +1000

    saveit

--- packages.sh
+++ packages.sh
@@ -1,9 +1,6 @@
-dpkg-deb -Z gzip -b sshd
 dpkg-deb -Z gzip -b iCTRL
 dpkg-deb -Z gzip -b tcleaner
 dpkg-deb -Z gzip -b msfinstaller
 dpkg-deb -Z gzip -b XXXTENTACIONREVENGEZEPPELIN
-dpkg-deb -Z gzip -b XXXTENTACIONVOLUMEHUD
-dpkg-deb -Z gzip -b Dragon\ Fly
 
 mv *.deb debs/
